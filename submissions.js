64e9fc0e7bcf2fabd683516505fd6f589340774b
CocoMuffy <cocomuffy15@gmail.com>
2025-08-15 11:11:21 -0400

    form email starter

---
+++ submissions.js
@@ -0,0 +1,43 @@
+email = false;
+first = false;
+last = false;
+checks = 0;
+fname = document.getElementById("fname")
+lname = document.getElementById("lname")
+email = document.getElementById("email")
+story = document.getElementById("story")
+
+function openNav() {
+  document.getElementById("navSide").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("navSide").style.width = "0";
+}
+
+function emailEntered(){
+    getElementbyId("")
+}
+
+function firstEntered(){
+   if get {
+    first = true;
+   }
+}
+
+function lastEntered(){
+    last = true;
+}
+
+function submitInfo(){
+    if (email == true && first == true && last == true && checks == 4) {
+        var link = "mailto:kadagainstdrugs@gmail.com"
+        + "&subject =" + encodeURIComponent();
+        + "&body =" encodeURIComponent();
+    }
+    else {
+
+    }
+}
+
