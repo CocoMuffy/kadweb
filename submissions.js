0bf38f2da357bc36ce643b1255a9e361bd588746
CocoMuffy <cocomuffy15@gmail.com>
2025-08-19 11:36:32 -0400

    idk man

--- submissions.js
+++ submissions.js
@@ -6,6 +6,10 @@ fname = document.getElementById("fname")
 lname = document.getElementById("lname")
 email = document.getElementById("email")
 story = document.getElementById("story")
+viable = document.getElementById("viablee")
+comfy = document.getElementById("comfyy")
+sober = document.getElementById("soberr")
+possible = document.getElementById("posiblee")
 
 function openNav() {
   document.getElementById("navSide").style.width = "250px";
@@ -21,22 +25,42 @@ function emailEntered(){
 }
 
 function firstEntered(){
-   if get {
+   if getElementbyId("") {
     first = true;
    }
 }
 
 function lastEntered(){
+   if getElementById(""){
     last = true;
+   } 
+}
+
+function addCheck(){
+    viable.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    alert('checked');
+  } else {
+    alert('not checked');
+  }
+})
+ comfy.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    alert('checked');
+  } else {
+    alert('not checked');
+  }
+})
 }
 
 function submitInfo(){
     if (email == true && first == true && last == true && checks == 4) {
         var link = "mailto:kadagainstdrugs@gmail.com"
-        + "&subject =" + encodeURIComponent();
-        + "&body =" encodeURIComponent();
+        + "&subject =" + encodeURIComponent("fname") + " " + encodeURIComponent("lname");
+        + "&body ="+ encodeURIComponent("story") + encodeURIComponent("email");
     }
     else {
+        
 
     }
 }
